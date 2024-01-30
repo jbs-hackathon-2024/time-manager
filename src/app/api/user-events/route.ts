c9625d14d55f8c2c03f95a9248325c7abe55de9c
Dalton Harrold <djharrold1@gmail.com>
2024-01-30 12:54:52 -0600

    barebones day-view

--- src/app/api/user-events/route.ts
+++ src/app/api/user-events/route.ts
@@ -1,6 +1,12 @@
+import { NextRequest } from 'next/server'
 import data from './scheduleItems.json'
 
 export const dynamic = 'force-dynamic' 
 export async function GET(request: Request) {
     return Response.json({data})
+}
+
+export async function PUT(request: NextRequest) {
+    console.log(request.body)
+    return Response.json({data: {}})
 }
