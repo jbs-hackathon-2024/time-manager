bfbbbafc9e74b311facf914ffeac2ea77989bc66
BobyWoby <bestdogeever865@gmail.com>
2024-02-04 11:54:58 -0600

    initial commit

--- src/app/add/calendar/page.tsx
+++ src/app/add/calendar/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+const ical = require('node-ical')
 
 const AddCalendarSchedule = () => {
+  // const events = ical.sync.parseFile('')
+  const CLIENT_ID = 'asdfasdklfhaslk';
+  const API_KEY = process.env.API_KEY
   return (
     <div>
         <Link href='/'>Back</Link>
         <form>
-          <input type='url'
+          <input type='url' value={"Add iCal link"}></input>
+          <button type="submit">Add Calendar</button>
         </form>
     </div>
   )
