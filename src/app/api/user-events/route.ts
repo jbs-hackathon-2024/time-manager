f5af8d94400bdc0ba51205816cdb75d9030519f6
BobyWoby <bestdogeever865@gmail.com>
2024-02-04 11:54:58 -0600

    initial commit

--- src/app/api/user-events/route.ts
+++ src/app/api/user-events/route.ts
@@ -2,7 +2,8 @@ import { NextRequest } from 'next/server'
 import data from './scheduleItems.json'
 
 export const dynamic = 'force-dynamic' 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest, { params }: { params: { cal_id: string }}) {
+    const calendar = fetch("https://www.googleapis.com/calendar/v3/calendars/" + params['cal_id'] + "/events?key=" + process.env.API_KEY)
     return Response.json({data})
 }
 
