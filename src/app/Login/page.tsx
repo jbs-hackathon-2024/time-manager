79d391a34d3a0333695de042e327c819234279ab
Jack O'Brien <jackobrienstl@gmail.com>
2024-02-18 11:28:59 -0600

    hi

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,29 +1,38 @@
 "use client";
 import React, { useState } from 'react'
 import './page.css';
+import Paper from '@mui/material/Paper'
 
 
 const Login = () => {
     const [ email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
         console.log(email, pass);
     }
 
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div className="stuff">
+            <h1 className="title">Sign in to FocusAI</h1>
+            <Paper elevation={3} className="box">
             <>
-            <h1>Sign in to _____</h1>
+            
             <form className="login-Form" onSubmit={handleSubmit}>
-            <label htmlFor="email" >Email</label>
-            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" onChange={(e) => setEmail(e.target.value)}/>
-            <label htmlFor="password">Password</label>
-            <input value={pass} type="password" placeholder="********" id="password" name="password" onChange={(e) => setPass(e.target.value)} />
-            <button type="submit">Log in</button>
+            <label htmlFor="email" className="label">Email</label>
+            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"  className="objects" onChange={(e) => setEmail(e.target.value)}/>
+            <label htmlFor="password" className="label">Password</label>
+            <input   value={pass} type="password" placeholder="********" id="password" name="password"  className="objects" onChange={(e) => setPass(e.target.value)} />
+            <button type="submit" className="objects redBtns">Log in</button>
             </form>
-            <button className="Reg-Btn">Don't have an account? Register here!</button></>
+            <button className="Reg-Btn objects " >Don't have an account? Register here!</button></>
+            </Paper>
         </div>
     )
 
